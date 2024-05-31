35f67b28e41dd784ed838adec8ccf4a520546995
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-05-31 14:10:12 +0800

    Initial import of scripts

---
+++ link.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+TARGET="$1"
+DIR_COMMONEXP="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+mkdir -pv "$TARGET"
+
+# Assuming zsh-Mordio is in ../zsh-Mordio
+if [[ ! -h "$TARGET/Mordio" ]]; then
+  ln -sTv "$DIR_COMMONEXP/../zsh-Mordio" "$TARGET/Mordio"
+fi
+
+if [[ ! -h "$TARGET/sc" ]]; then
+  ln -sTv "$DIR_COMMONEXP/steps" "$TARGET/sc"
+fi
+if [[ ! -h "$TARGET/uc" ]]; then
+  ln -sTv "$DIR_COMMONEXP/utils" "$TARGET/uc"
+fi
+
+# Make a "relink" script
+if [[ ! -x "$TARGET/link.again.sh" ]]; then
+  rm -fv "$TARGET/link.again.sh"
+  cat > "$TARGET/link.again.sh" <<EOF
+#!/usr/bin/bash -v
+set -euo pipefail
+
+EOF
+  printf '"%s"' "$(readlink -f "$0")" >> "$TARGET/link.again.sh"
+  printf ' "%s"' "$@" >> "$TARGET/link.again.sh"
+  chmod 755 "$TARGET/link.again.sh"
+fi
---
+++ utils/csv2html.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2020-2024, Hojin Koh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Convert csv table from stdin to html table, with first row being headers
+# Opinionated: html indent at 2 spaces
+
+import csv
+import sys
+
+def main():
+    fp = csv.reader(sys.stdin)
+
+    print("<table>\n  <thead>")
+    isHeaderPrinted = False
+
+    for row in fp:
+        print("    <tr>")
+        print("      ", end='')
+        for val in row:
+            if not isHeaderPrinted:
+                print(F"<th>{val}</th>", end='')
+            else:
+                print(F"<td>{val}</td>", end='')
+        print("\n    </tr>")
+        if not isHeaderPrinted:
+            print("  </thead>\n  <tbody>")
+            isHeaderPrinted = True
+
+    print("  </tbody>\n</table>")
+
+if __name__ == "__main__":
+    main()
+

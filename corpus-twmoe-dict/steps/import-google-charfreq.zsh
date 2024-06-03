bc40b3d4575a8bb8ec788fc441c7a1d480d2e341
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-06-03 11:49:45 +0800

    Import google ngram's character frequency

---
+++ corpus-twmoe-dict/steps/import-google-charfreq.zsh
@@ -0,0 +1,36 @@
+#!/usr/bin/env zsh
+# Copyright 2020-2024, Hojin Koh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+description="Import google chinese character frequency table from google n-gram at https://storage.googleapis.com/books/ngrams/books/datasetsv3.html"
+
+setupArgs() {
+  opt -r in '' "Original Data Archive"
+  opt -r out '' "Output table"
+  optType out output table
+}
+
+main() {
+  if [[ ! -f "$in" ]]; then
+    info "Downloading the data from google ..."
+    curl -L -o "$in" 'http://storage.googleapis.com/books/ngrams/books/20200217/chi_sim/1-00000-of-00001.gz'
+  fi
+
+  gunzip -c "$in" \
+  | perl -CSAD -nle 'print if /^\p{Han}\t/' \
+  | us/parse-google-charfreq.py \
+  | out::save
+  return $?
+}
+
+source Mordio/mordio

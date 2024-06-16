9a1d6fab70c5f9c09870520dc1a00946ad9610e5
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-06-16 22:53:13 +0800

    Fix the script description of sentence-splitting script

--- steps/text-split-sent-zh.zsh
+++ steps/text-split-sent-zh.zsh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-description="A Test Script"
+description="Splitting Chinese documents into sentences based on punctuation and newlines"
 dependencies=( "uc/text-split-sent-zh.py" )
 
 setupArgs() {

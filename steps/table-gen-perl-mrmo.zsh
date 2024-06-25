85927226dd2fc0d4c42241e673b0fdbfa91af0d6
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-06-25 17:04:26 +0800

    Separate the MIMO version of table-gen (default) and multi-rule version

--- steps/table-gen-perl-mrmo.zsh
+++ steps/table-gen-perl-mrmo.zsh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-description="Generate a new table based on ids of another table and a perl conversion rule (MIMO)"
+description="Generate a new table based on ids of another table and a perl conversion rule (MRMO)"
 dependencies=()
 importantconfig=(rule)
 
@@ -20,8 +20,8 @@ setupArgs() {
   opt -r out '()' "Output table"
   optType out output table
   
-  opt -r rule '' "Perl conversion rule"
-  opt -r in '()' "Input table"
+  opt -r rule '()' "Perl conversion rule"
+  opt -r in '' "Input table"
   optType in input table
 }
 
@@ -30,27 +30,26 @@ main() {
     err "Unreal table output not supported" 15
   fi
 
-  if [[ $#out != $#in ]]; then
-    err "Input and Output must have the same number of parameters" 15
+  if [[ $#out != $#rule ]]; then
+    err "Rule and Output must have the same number of parameters" 15
   fi
 
   local nr
   local i
-  info "ID conversion rule: $rule"
   for (( i=1; i<=$#out; i++ )); do
-    info "Processing file set $i/$#in: ${in[$i]}"
+    info "ID conversion rule: ${rule[$i]}"
     getMeta in $i nRecord nr
 
     if out::isReal $i; then
-      in::load $i \
-      | perl -CSAD -nle "$rule" \
+      in::load \
+      | perl -CSAD -nle "${rule[$i]}" \
       | lineProgressBar $nr \
       | out::save $i
       if [[ $? != 0 ]]; then return $?; fi
     else
       (
-        in::getLoader $i
-        printf " | perl -CSAD -nle '%s'" "$rule"
+        in::getLoader
+        printf " | perl -CSAD -nle '%s'" "${rule[$i]}"
       ) | out::save $i
       if [[ $? != 0 ]]; then return $?; fi
     fi

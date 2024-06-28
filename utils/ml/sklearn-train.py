2618d2bf97704e5f78b7a1593d87d1e6865458b5
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-06-28 15:59:42 +0800

    Delete some temporary debug things

--- utils/ml/sklearn-train.py
+++ utils/ml/sklearn-train.py
@@ -54,9 +54,7 @@ def main():
     print(F"Training labels: {len(aLabels)}", file=sys.stderr)
 
     if typeModel == "rf":
-        from sklearn.svm import SVC
-        #objModel = SVC(C=0.5)
-        objModel = RandomForestClassifier(verbose=1, n_jobs=int(os.getenv("OMP_NUM_THREADS", 3)), max_samples=0.4, n_estimators=250, min_samples_split=6, **paramModel)
+        objModel = RandomForestClassifier(verbose=1, n_jobs=int(os.getenv("OMP_NUM_THREADS", 3)), oob_score=True, n_estimators=250, **paramModel)
         objModel.fit(mtxFeats, aLabels)
 
     objOnnx = to_onnx(objModel, mtxFeats[:1])

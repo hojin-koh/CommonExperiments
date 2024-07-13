71ca073ace14e0bcc097d2b02b4f308e43525c71
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-07-13 14:16:40 +0800

    MIMO-enable two more scripts

--- steps/normalize-unicode-key.zsh
+++ steps/normalize-unicode-key.zsh
@@ -12,32 +12,39 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-description="Normalize unicode in the keys of a table"
+description="Normalize unicode in the keys of a table (MIMO possible)"
 dependencies=( "uc/normalize-unicode.py" "uc/table-merge.py" "uc/interpolate-count.py" )
 importantconfig=(mode)
 
 setupArgs() {
-  opt -r out '' "Output table"
+  opt -r out '()' "Output table"
   optType out output table
 
-  opt -r in '' "Input table"
+  opt -r in '()' "Input table"
   optType in input table
-  opt -r conv '' "Input character mapping table"
+  opt -r conv '()' "Input character mapping table"
   optType conv input table
 
-  opt mode 'merge' "How to deal with duplicated keys, merge or interpolate"
+  opt mode '(merge)' "How to deal with duplicated keys, merge or interpolate"
 }
 
 main() {
-  if ! out::isReal; then
+  if ! out::ALL::isReal; then
     err "Unreal table output not supported" 15
   fi
 
-  in::load \
-  | uc/normalize-unicode.py <(conv::load) key \
-  | if [[ "$mode" == "merge" ]]; then uc/table-merge.py --set; else uc/interpolate-count.py 1.0 /dev/stdin; fi \
-  | out::save
-  return $?
+  computeMIMOStride out in conv mode
+
+  local i
+  for (( i=1; i<=$#out; i++ )); do
+    computeMIMOIndex $i out in conv mode
+
+    in::load $INDEX_in \
+    | uc/normalize-unicode.py <(conv::load $INDEX_conv) key \
+    | if [[ "${mode[$INDEX_mode]}" == "merge" ]]; then uc/table-merge.py --set; else uc/interpolate-count.py 1.0 /dev/stdin; fi \
+    | out::save $i
+    if [[ $? != 0 ]]; then return 1; fi
+  done
 }
 
 source Mordio/mordio
--- steps/table-interpolate.zsh
+++ steps/table-interpolate.zsh
@@ -12,12 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-description="Merge counts or scores from multiple tables"
+description="Merge counts or scores from multiple tables (MIMO Possible)"
 dependencies=( "uc/interpolate-count.py" )
 importantconfig=(normalize w)
 
 setupArgs() {
-  opt -r out '' "Output table"
+  opt -r out '()' "Output table"
   optType out output table
 
   opt -r in '()' "Input tables"
@@ -28,22 +28,31 @@ setupArgs() {
 }
 
 main() {
+  computeMIMOStride in out w
+
+  local params=()
   local i
-  local param="uc/interpolate-count.py"
-  if [[ "$normalize" == "true" ]]; then
-    param+=" --normalize"
-  fi
-  for (( i=1; i<=${#in[@]}; i++ )); do
-    param+=" ${w[$i]-1.0} <($(in::getLoader $i))"
+  for (( i=1; i<=$#in; i++ )); do
+    computeMIMOIndex $i in out w
+
+    if [[ $INDEX_out -gt $#params ]]; then
+      params[$INDEX_out]="uc/interpolate-count.py"
+      if [[ "$normalize" == "true" ]]; then
+        params[$INDEX_out]+=" --normalize"
+      fi
+    fi
+    params[$INDEX_out]+=" ${w[$INDEX_w]-1.0} <($(in::getLoader $i))"
   done
 
-  if out::isReal; then
-    eval "$param" | out::save
-    return $?
-  fi
-
-  echo "$param" | out::save
-  return $?
+  for (( i=1; i<=$#out; i++ )); do
+    if out::isReal $i; then
+      eval "${params[$i]}" | out::save $i
+      if [[ $? != 0 ]]; then return 1; fi
+    else
+      echo "${params[$i]}" | out::save $i
+      if [[ $? != 0 ]]; then return 1; fi
+    fi
+  done
 }
 
 source Mordio/mordio
